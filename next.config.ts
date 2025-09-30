e65fded6f52735eb5010648d05b324ef1cc6de02
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-09-30 22:55:26 +0200

    Vercel-fix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
-  eslint: {
-    ignoreDuringBuilds: true, 
-  },
-
 };
 
 export default nextConfig;
